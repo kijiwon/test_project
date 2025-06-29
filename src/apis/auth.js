8f78ddb4d348849e8eec414596c93ca045cc6c81
kijiwon <kiji1313@gmail.com>
2025-06-29 18:55:55 +0900

    토큰 재발급

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { useAuthStore } from "../store/auth";
 import { setCookie } from "./cookie";
 
+const { updateAccessToken, updateTokenExp } = useAuthStore.getState();
+
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: { "Content-Type": "application/json" },
@@ -19,7 +21,6 @@ export const signupAPI = async (formData) => {
 export const signinAPI = async (formData) => {
   try {
     const res = await api.post("/signin", formData);
-    const { updateAccessToken, updateTokenExp } = useAuthStore.getState();
 
     const accessToken = await res.data.accessToken;
     const refreshToken = await res.data.refreshToken;
@@ -37,15 +38,26 @@ export const signinAPI = async (formData) => {
   }
 };
 
-export const refreshAPI = async (refreshToken) => {
+export const refreshAPI = async (token) => {
   try {
-    const res = await api.post("/refresh", refreshToken, {
+    const res = await api.post("/refresh", token, {
       headers: {
         "Content-Type": "application/json",
       },
     });
+
+    const accessToken = await res.data.accessToken;
+    const refreshToken = await res.data.refreshToken;
+    // 만료시간
+    const payload = JSON.parse(atob(accessToken.split(".")[1]));
+    // console.log("payload>>>", payload);
+    const tokenExp = new Date(payload.exp * 1000);
+
+    updateAccessToken(accessToken);
+    updateTokenExp(tokenExp);
+    setCookie("refreshToken", refreshToken, { path: "/" });
     return res;
   } catch (err) {
-    return err;
+    return err.response.data.message;
   }
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,14 +18,14 @@ export default function Home() {
       //로그인 정보 존재
 
       if (new Date(tokenExp) - new Date(now) - 5000 <= 0) {
-        console.log("토큰요청>>>");
         if (refreshToken !== undefined) {
           const res = await refreshAPI({ refreshToken: refreshToken });
-          console.log(res);
+          if (res.status !== 200) {
+            alert("토큰이 만료되었습니다. 다시 로그인해 주세요");
+          }
         }
-      } else {
-        setIsLoggedIn(true);
       }
+      setIsLoggedIn(true);
     } else {
       setIsLoggedIn(false);
     }
@@ -37,14 +37,12 @@ export default function Home() {
 
   useEffect(() => {
     if (!tokenExp) return;
-    console.log("현재 시간>>", new Date());
     console.log("만료시간>>", new Date(tokenExp));
     const tokenTimeout =
       new Date(tokenExp).getTime() - new Date().getTime() - 5000;
 
     console.log("남은 시간>>", tokenTimeout);
     const timer = setTimeout(() => {
-      console.log("----end----");
       checkLogin();
     }, tokenTimeout);
     return () => clearTimeout(timer);
